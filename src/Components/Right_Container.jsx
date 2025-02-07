e9366f6b31dd0f709b43c8e68310b7b5de01dd2a
AKaushik2710 <amankaushik2710ak@gmail.com>
2025-02-07 16:39:39 +0000

    7FEB

--- src/Components/Right_Container.jsx
+++ src/Components/Right_Container.jsx
@@ -25,16 +25,15 @@ export default function Right_Container({functionality}){
             <Span id="saver" clickHandler={() => handleFolderDisplay(true)} >&#10003;</Span>
             </Div>
             <Div id="text_fol" cn="text_mod">{/* Folderated Notes Display */}
-                {!choices.folder_files ?(choices.change.val ? (<> {folders.map(folder =>{
+                {!choices.folder_files ?<>{choices.change.val ? (folders.map(folder =>{
                     folder.files.map(file =>{
                         return <Para id={folder.id} cn="note" key={folder.id}>{file}<Span cn="dustbin" clickHandler={(e)=>{e.stopPropagation();handleFolderDeletion(folder.id);}}>&#128465;</Span></ Para>
                     })
-                })}
-                <Para id="add" clickHandler={()=> handleFileAddition({type : "display", val : true})}>&#43;</Para></>):(folder_files.map(file => {
+                })):(folder_files.map(file => {
                     if(file.checked){
                         return <Para id={file.id} key={file.id}>{file.para}</Para>
                     }
-                }))) : (
+                }))}<Para id="add" clickHandler={()=> handleFileAddition({type : "display", val : true})}>&#43;</Para></> : (
                     <Div>
                         <Para cn="note">
                             <Span clickHandler={()=>{handleFileAddition({type : "back", val : true})}}>
